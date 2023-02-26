aaca2b1d4bea173cb47716af5c5db9db88d84cee
TFesta1 <festat1@hawkmail.newpaltz.edu>
2023-02-26 11:21:52 -0500

    Refs to create reactive values (HomeView.vue)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@
     Home
     <p ref="p">name {{ name }} age {{ age }}</p>
     <button @click="handleClick">click</button>
+    <button @click="age++">Add1 to age</button>
+    <input type="text" v-model="name"> <!-- v-model implements a two-way binding-->
   </div>
 </template>
 
@@ -14,20 +16,15 @@ import { ref } from 'vue'
 export default {
   name: 'HomeView',
   setup() {
-    console.log("setup")
 
-    let name = 'mario'
-    let age = 30
-
-    const p = ref(null)
-
+    const name = ref('mario') //ref's are reactive values
+    const age = ref(30)
+    
     const handleClick = () => {
-      console.log(p, p.value)
-      p.value.classList.add('test') //normal js property classList
-      p.value.textContent = 'hello, ninjas'
+      name.value = 'luigi' //makes it reactive
     }
 
-    return { name, age, handleClick, p }
+    return { name, age, handleClick }
   },
   data() {
     return {
