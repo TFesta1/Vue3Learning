7dc5f4782a239a55bbb0ab468f3e0f8b524feac6
TFesta1 <festat1@hawkmail.newpaltz.edu>
2023-02-26 16:09:19 -0500

    UsingProps

---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import SinglePost from "./SinglePost.vue"
+export default {
+    name: 'Comp3BlogPostList',
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+        console.log(props.posts)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="home">
     Home
-    <input type="text" v-model="search">
-    <p>searchterm: {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      {{ name }}
-    </div>
-
-    <button @click="handleClick">Stop watching</button>
+    <PostList :posts="posts"/>
+    
     
   </div>
 </template>
@@ -16,36 +11,18 @@
 // @ is an alias to /src
 
 import { ref, computed, watch, watchEffect } from 'vue'
+import PostList from '../components/PostList.vue'
 
 export default {
   name: 'HomeView',
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref(['mario', 'yoshi', 'koopa', 'bowser'])
+    const posts = ref([
+      { title: "welcome", body: "hello", id: 1},
+      { title: "not welcome", body: "goodbye", id: 2}
+    ])
 
-
-    // Whenever search changes
-    const stopWatch = watch(search, () => {
-      console.log('watched')
-    })
-
-
-    // Runs initially, and watches any dependencies inside of it (search.value)
-    const stopEffect = watchEffect(() => {
-      console.log("watchEffect", search.value)
-    })
-    
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      // Stops it from watching
-      stopWatch()
-      stopEffect()
-    }
-
-    return { matchingNames, search, handleClick }
+    return { posts }
   }
   
   
